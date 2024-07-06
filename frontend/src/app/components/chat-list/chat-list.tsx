e4e09aca40212606e8d1ef3834e45608ee354959
Виктор Дрыкин <drykin@qtim.ru>
2024-07-06 16:31:24 +0300

    Get chats list from backend

--- frontend/src/app/components/chat-list/chat-list.tsx
+++ frontend/src/app/components/chat-list/chat-list.tsx
@@ -1,24 +1,33 @@
 import styles from "./chat-list.module.css";
-function ChatItem() {
+// todo types
+function ChatItem({chatInfo}) {
     return (
         <li className={styles.item} data-chat="668144e920bf5b9d20163c42">
-            <img className={styles.img} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg"/>
-            <span className={styles.name}>Thomas Bangalter</span>
+            <img className={styles.img} src={chatInfo.avatar} alt='avatar'/>
+            <span className={styles.name}>{chatInfo.name}</span>
             <span className={styles.time}></span>
             <span className={styles.preview}></span>
         </li>
-    )
+    );
 }
 
-export default function ChatList() {
+export default async function ChatList() {
+    const chatList = await getChatList();
+    const chatListItems = chatList.map((item: any) => <ChatItem key={item.id} chatInfo={item}/>);
+
     return (
         <ul id="chatList" className={styles.list}>
-            <ChatItem/>
-            <ChatItem/>
-            <ChatItem/>
-            <ChatItem/>
-            <ChatItem/>
-            <ChatItem/>
+            {chatListItems}
         </ul>
-    )
+    );
+}
+
+async function getChatList() {
+    const res = await fetch('http://localhost:3000');
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
 }
